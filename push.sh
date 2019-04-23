17c9c500f73135fcd7c1ff1f6dfe74b9dd9d422e
Liu Jianwei <liujianwei@patsnap.com>
2019-04-23 13:54:45 +0800

    update scripts

---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+docker push registry-vpc.cn-hangzhou.aliyuncs.com/shawn_private/employee:1.1
+docker push registry-vpc.cn-hangzhou.aliyuncs.com/shawn_private/department:1.1
+docker push registry-vpc.cn-hangzhou.aliyuncs.com/shawn_private/organization:1.1
