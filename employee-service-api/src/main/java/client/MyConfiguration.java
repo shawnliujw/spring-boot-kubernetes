c2eaea036280cad73b6086c01353bca7e86cb657
Liu Jianwei <liujianwei@patsnap.com>
2019-04-23 17:02:41 +0800

    test spring.factories

--- employee-service-api/src/main/java/client/MyConfiguration.java
+++ employee-service-api/src/main/java/client/MyConfiguration.java
@@ -3,6 +3,7 @@ package client;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Configuration
