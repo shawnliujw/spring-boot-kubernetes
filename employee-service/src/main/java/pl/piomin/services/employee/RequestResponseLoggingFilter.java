9a7d58bb9dc2c936d61e13ab07da69250678c534
Liu Jianwei <liujianwei@patsnap.com>
2019-04-23 14:31:19 +0800

    add filter

---
+++ employee-service/src/main/java/pl/piomin/services/employee/RequestResponseLoggingFilter.java
@@ -0,0 +1,21 @@
+package pl.piomin.services.employee;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class RequestResponseLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        System.out.println(httpServletRequest.getRequestURL());
+        doFilter(httpServletRequest,httpServletResponse,filterChain);
+    }
+}
