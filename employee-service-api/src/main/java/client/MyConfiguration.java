9e2f9b78acf5d083a34f6bb2ea08a091726bcba5
Liu Jianwei <liujianwei@patsnap.com>
2019-04-23 13:02:45 +0800

    enable auto configuration for jar project

---
+++ employee-service-api/src/main/java/client/MyConfiguration.java
@@ -0,0 +1,9 @@
+package client;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class MyConfiguration {
+}
