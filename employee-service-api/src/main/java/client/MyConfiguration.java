0b8c3c3a937e2d8aa170555725274ea9aaa735bf
Liu Jianwei <liujianwei@patsnap.com>
2019-04-23 17:20:04 +0800

    test spring.factories

--- employee-service-api/src/main/java/client/MyConfiguration.java
+++ employee-service-api/src/main/java/client/MyConfiguration.java
@@ -2,6 +2,7 @@ package client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Component;
 @EnableAutoConfiguration
 public class MyConfiguration {
 
-    @Autowired
+    @Bean
     public  void test() {
         System.out.println("=======================");
         System.out.println("=======================");
