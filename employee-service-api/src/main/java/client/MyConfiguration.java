0bb6623208d0ebb21cc46632cd90cd12009eb424
Liu Jianwei <liujianwei@patsnap.com>
2019-04-23 16:49:32 +0800

    remove factory.properties

--- employee-service-api/src/main/java/client/MyConfiguration.java
+++ employee-service-api/src/main/java/client/MyConfiguration.java
@@ -1,8 +1,11 @@
 package client;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@ComponentScan(basePackages = "client")
 public class MyConfiguration {
 }
