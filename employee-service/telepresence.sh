1408899055d62af199575cc193ad419974957652
Liu Jianwei <liujianwei@patsnap.com>
2019-04-24 16:52:01 +0800

    submit telepresense.sh

--- employee-service/telepresence.sh
+++ employee-service/telepresence.sh
@@ -6,12 +6,12 @@ telepresence \
     --docker-run \
     --rm \
     -v $(pwd):/build \
-    -v $HOME/.m2/repository:/m2 \
+    -v $HOME/.m2/repository:/m3 \
     -v=/tmp/known/var/run/secrets:/var/run/secrets \
     -p 8080:8080 \
     maven:3.6-jdk-8-alpine \
         mvn \
             Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n" \
-            -Dmaven.repo.local=/m2 \
+            -Dmaven.repo.local=/m3 \
             -f /build \
             spring-boot:run
---
+++ init-ingress.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+ kubectl apply -f kubernetes/ingress-default-backend.yaml
+ kubectl apply -f kubernetes/ingress-daemon.yaml
