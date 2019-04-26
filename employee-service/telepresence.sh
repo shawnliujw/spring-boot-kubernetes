bb3f14ef861accd3a9e9121cdeb10fddcdd29a28
Shawn Liu <shawn.jw.liu@gmail.com>
2019-04-26 14:13:00 +0800

    adjust module order

--- employee-service/telepresence.sh
+++ employee-service/telepresence.sh
@@ -1,18 +1,2 @@
 #!/usr/bin/env bash
-
-telepresence \
-    --mount /tmp/known \
-    --swap-deployment employee \
-    --docker-run \
-    --rm \
-    -v $(pwd):/build \
-    -v $HOME/.m2/repository:/m3 \
-    -v=/tmp/known/var/run/secrets:/var/run/secrets \
-    -p 8080:8080 \
-    -p 5005:5005 \
-    -e MAVEN_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 \
-    maven:3-jdk-8  \
-        mvn \
-            -Dmaven.repo.local=/m3 \
-            -f /build \
-            spring-boot:run
+telepresence --swap-deployment employee --env-json service_env.json
